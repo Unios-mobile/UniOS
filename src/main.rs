4883674395fabfc106ac5a60c66c379c5c3b1beb
MicroRay620 <microray620@gmail.com>
2026-01-12 20:16:35 -0500

    Added more documentation to the main.rs
    Added more description to the main.rs documentation

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,33 @@
  * // What is UniOS? //
  * UniOS is a Linux mobile OS that specializes in giving the user freedom to do and run anything.
  * To achieve this multiple file executables are supported. This includes but not limited to:
- * * apk // This is the Android package 
+ * Most of these (besides unx and apk) won't be supported in the first few iterations
+ * * apk // This is the Android package
+ * * AppImage // This is a universal package type for Linux distros
+ * * * Not all AppImage files will work, check to ensure what will work
  * * deb // This is the Debian/Ubuntu package 
  * * exe // This is the Windows package 
+ * * nix // This is the NixOS package 
  * * pkg.tar.zst // This is the Arch Linux package
  * * rpm // This is the Fedora package 
  * * unx // This is the UniOS package
+ * Unsupported Package Types
+ * * flatpak // No reliable way to have them on mobile 
+ * * * This will change in the future
+ * * snap // Same as flatpak and not well liked in the Linux community
  */
 /*
  * This is for rendering the kernel
- * In here the Linux Kernel [https://github.com/torvalds/linux] is ported to UniOS
+ * In here the /e/OS [https://gitlab.e.foundation/e/os] is ported to UniOS
+ * * To support the functions of UniOS, there is custom daemons added to /e/OS to allow multiple
+ * * * package types
  * Crates will be added in development as things progress
  */
+// Imports 
+ 
+// Functions 
+fn daemon_render() {
+}
+// Main function 
 fn main() {
-    println!("Hello, world!");
 }
